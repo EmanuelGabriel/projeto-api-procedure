26f6ac6b0913fb7ba2f977c3e7ec84af7f4865a5
Emanuel Sousa <emanuel.sousa@mj.gov.br>
2021-07-12 20:02:49 -0300

    primeiro commit

---
+++ src/main/java/br/com/emanuelgabriel/projetoapiprocedure/ProjetoApiProcedureApplication.java
@@ -0,0 +1,13 @@
+package br.com.emanuelgabriel.projetoapiprocedure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiProcedureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiProcedureApplication.class, args);
+	}
+
+}
